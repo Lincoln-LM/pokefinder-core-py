6988296d731ecf694c6c3819e556214b66a621e1
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2023-11-29 20:22:48 -0700

    include PF license in src distribution

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ ext_modules = [
     Extension(
         "core/**/*",
         ["core/**/*.pyx"],
-        depends=list(glob.glob("core/PokeFinder/Source/Core/**/*.*pp", recursive=True)),
+        depends=list(glob.glob("core/PokeFinder/Source/Core/**/*.*pp", recursive=True))
+        + ["core/PokeFinder/LICENSE"],
         extra_compile_args=["-std=c++20"],
         extra_link_args=["-std=c++20"],
         include_dirs=["core/PokeFinder/Source"],
